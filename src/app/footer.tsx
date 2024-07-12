14d1d10d52b3d2f65e45755091d575301a323b3a
Anthony Martini <anthonycm2022@gmail.com>
2024-07-12 18:56:31 -0400

    Fixed white text on dark mode

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <div className="static w-full h-[80px] flex flex-rpw text-center items-center text-[14px] text-slate-600">
+    <div className="static w-full h-[80px] flex flex-rpw text-center items-center text-[14px] text-slate-600 bg-slate-50">
       <div className="flex-1">Created by Anthony Martini, 2024</div>
       <div className="flex-1 flex justify-center">
         <a
